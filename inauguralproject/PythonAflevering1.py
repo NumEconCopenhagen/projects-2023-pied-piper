6d593f5e7b89e0446d4978a139bee176d92aac8f
snk411 <snk441@alumni.ku.dk>
2023-03-22 10:01:44 +0100

    lavet t.o.m. opg 3

--- inauguralproject/PythonAflevering1.py
+++ inauguralproject/PythonAflevering1.py
@@ -25,7 +25,7 @@ class HouseholdSpecializationModelClass:
 
         # c. household production
         par.alpha = 0.5
-        par.sigma = 1
+        par.sigma = 1.0
 
         # d. wages
         par.wM = 1.0
@@ -62,7 +62,7 @@ class HouseholdSpecializationModelClass:
             H = np.minimum(HM, HF)
 
         else:
-            H = ((1-par.alpha) * HM ** ((par.sigma-1)/par.sigma) + par.alpha * HF ** ((par.sigma-1)/par.sigma)) ** (par.sigma/(par.sigma-1))
+            H = ((1-par.alpha) * HM ** ((par.sigma-1)/par.sigma) + par.alpha * HF ** ((par.sigma-1)/par.sigma)) ** ((par.sigma)/(par.sigma-1))
 
         # c. total consumption utility
         Q = C**par.omega*H**(1-par.omega)
@@ -114,25 +114,95 @@ class HouseholdSpecializationModelClass:
 
         return opt
 
-    def solve(self,do_print=False):
+    def solve_continously(self,do_print=False):
         """ solve model continously """
         
+        par = self.par
+        sol = self.sol
+        opt = SimpleNamespace()
+
+        # Define the objective for our model
+        def objective(x):
+            LM,HM,LF,HF = x
+            return -self.calc_utility(LM,HM,LF,HF)
+        
+        
+
+        # Firstly we define the constraints for both male and female 
+        def constraint1(x):
+            LM,HM,LF,HF = x   
+            return 24-(LM+HM)
+
+        def constraint2(x):
+            LM,HM,LF,HF = x
+            return 24-(LF+HF)
+
+        
+        # Create intial guess for our model to use
+        initial_guess = [12,12,12,12]
     
-        pass   
+
+        # We combine our two constraints using inequality 
+        constraints = [{"type": "ineq", "fun": constraint1}, 
+                        {"type": "ineq", "fun": constraint2}]
+        
+        
+        result = optimize.minimize(objective, initial_guess, 
+                                   method='SLSQP',constraints=constraints)
+        
+        sol.LM = result.x[0]
+        sol.HM = result.x[1]
+        sol.LF = result.x[2]
+        sol.HF = result.x[3]
+
+       
+
+        
+    
+        
+        
+
+    
+ 
+
+     
 
     def solve_wF_vec(self,discrete=False):
         """ solve model for vector of female wages """
+        var_WF = [0.8,0.9,1.0,1.1,1.2]
 
-        pass
+        list_HFHM_q4 = []
+        list_WFWM_q4 = []
+
+        for w in var_WF :
+            model = HouseholdSpecializationModelClass()
+            model.par.wF = w
+            model.solve_continously()
+
+            HFHM = model.sol.HF/model.sol.HM #HFHM forhold defineres
+            list_HFHM_q4.append(HFHM)
+
+
+            WFWM = w/model.par.wM # vi finder forholdet mellem wF og wM
+            list_WFWM_q4.append(WFWM)
+
+        log_sol_WFWM_q4 = np.log(list_WFWM_q4) # Der tages log til forholdene
+        log_sol_HFHM_q4 = np.log(list_HFHM_q4)
+            
+
+        
+
+
+        
 
     def run_regression(self):
         """ run regression """
 
         par = self.par
         sol = self.sol
-
-        x = np.log(par.wF_vec)
-        y = np.log(sol.HF_vec/sol.HM_vec)
+        
+        x = np.log(par.wF)
+        y = np.log(sol.HF/sol.HM)
         A = np.vstack([np.ones(x.size),x]).T
         sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0]
     
